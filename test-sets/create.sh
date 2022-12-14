1547b28dc1e999c25184e90fbd8fa43dd791d388
Matt Post <mattpost@microsoft.com>
2022-12-14 16:56:46 +0000

    remove trailing <eos> from target context

--- test-sets/create.sh
+++ test-sets/create.sh
@@ -5,5 +5,5 @@ for set in anaphora lexical_choice; do
 done
 
 for set in anaphora lexical_choice; do
-    paste $set.prev.en $set.current.en $set.prev.fr $set.current.fr | perl -pe "s/\t/ <eos>/; s/\t/MATT/; s/\t/ <eos>\t/; s/MATT/\t/" | cut -f1-2 > decode.$set
+    paste $set.prev.en $set.current.en $set.prev.fr | perl -pe "s/\t/ <eos>/" > decode.$set
 done
